936626d8cdafb792f095af1ac9c0c1e12c9dacd6
Shahnawaz Noor <shahnawaznoor2020@gmail.com>
2026-01-12 19:28:06 +0530

    Added Task 1 and Task 2

---
+++ Assignment1.1.py
@@ -0,0 +1,12 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+sum = num1 + num2
+difference = num1 - num2
+product = num1 * num2
+division = num1 / num2
+floor_division = num1 // num2
+print("The Sum of ",num1,"and",num2,"is: ",sum)
+print("The Difference of ",num1,"and",num2,"is: ",difference)
+print("The Product of ",num1,"and",num2,"is: ",product)
+print("The Division of ",num1,"and",num2,"is: ",division)
+print("The Floor Division of ",num1,"and",num2,"is: ",floor_division)
---
+++ Assinment1.2.py
@@ -0,0 +1,4 @@
+first=input("Enter the first Name :")
+second=input("Enter the second Name :")
+full_name=first+" "+second
+print("Hello ,",full_name,"! Welcome to Python program")
